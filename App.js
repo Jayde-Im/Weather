6d372e27218359234575281498ef4ecaa7f5e041
Jayde-Im <jeongminjim@gmail.com>
2018-03-28 23:03:32 +0900

    [Add] Weather VIew

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Image, ActivityIndicator } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Weather from "./Weather";
 
 export default class App extends Component {
   state = {
-    isLoaded: false
+    isLoaded: false,
+    error: null
   };
 
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          isLoaded: true
+        });
+      },
+      error => {
+        this.setState({
+          error: error
+        });
+      }
+    );
+  }
+
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
 
     return (
       <View style={styles.container}>
-        {isLoaded ? null : (
+        <StatusBar hidden={true} />
+        {isLoaded ? (
+          <Weather />
+        ) : (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>
         )}
       </View>
@@ -26,6 +47,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff"
   },
+  errorText: {
+    color: "red",
+    marginBottom: 40
+  },
   loading: {
     flex: 1,
     backgroundColor: "#FDF6AA",
@@ -34,6 +59,6 @@ const styles = StyleSheet.create({
   },
   loadingText: {
     fontSize: 38,
-    marginBottom: 100
+    marginBottom: 24
   }
 });
