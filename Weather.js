4ada00b3e7d18252c1618a8a02886e986f6be635
Jayde-Im <jeongminjim@gmail.com>
2018-04-04 20:53:52 +0900

    [Modify] Changing the icons

--- Weather.js
+++ Weather.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo";
-import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
 
 const weatherCases = {
@@ -9,43 +9,49 @@ const weatherCases = {
     colors: ["#00C6FB", "#005BEA"],
     title: "Raining like cats and dogs",
     subtitle: "For more info look outside",
-    icon: "ios-rainy"
+    icon: "weather-rainy"
   },
   Clear: {
     colors: ["#FEF253", "#FF7300"],
     title: "Sunny",
     subtitle: "Go have a sun bath",
-    icon: "ios-sunny"
+    icon: "weather-sunny"
   },
   Thunderstorm: {
     colors: ["#00ECBC", "#007ADF"],
     title: "Thunderstorm in the house",
     subtitle: "Actually, outside of the house",
-    icon: "ios-thunderstorm"
+    icon: "weather-lightning"
   },
   Clouds: {
     colors: ["#D7D2CC", "#304352"],
     title: "Clouds",
     subtitle: "I know, super boring",
-    icon: "ios-cloudy"
+    icon: "weather-cloudy"
   },
   Snow: {
     colors: ["#7DE2FC", "#B9B6E5"],
     title: "Cold",
     subtitle: "Do you want to build a snowman?",
-    icon: "ios-snow"
+    icon: "weather-snowy"
   },
   Drizzle: {
     colors: ["#89F7FE", "#66A6FF"],
     title: "Drizzle",
     subtitle: "Is like rain",
-    icon: "ios-rainy-outline"
+    icon: "weather-hail"
   },
   Haze: {
     colors: ["#89F7FE", "#66A6FF"],
     title: "Haze",
     subtitle: "Mist",
-    icon: "ios-rainy-outline"
+    icon: "weather-hail"
+  },
+  Mist: {
+    colors: ["#89F7FE", "#66A6FF"],
+    title: "Mist!",
+    subtitle: "It's like you have no glasses on",
+    icon: "weather-fog"
   }
 };
 
@@ -56,7 +62,7 @@ function Weather({ weatherName, temp }) {
       style={styles.container}
     >
       <View style={styles.upper}>
-        <Ionicons
+        <MaterialCommunityIcons
           color="white"
           size={144}
           name={weatherCases[weatherName].icon}
